656b965a0cfbc8acaf79098ac90dd5a59e172109
jpward <Ward.j2@Gmail.com>
2019-09-26 09:43:20 -0400

    Updating to replace existing user/groups before adding new

--- restrict_useradd.sh
+++ restrict_useradd.sh
@@ -37,8 +37,8 @@ elif [ $gid -eq 0 ]; then
 else
     makehome="-m"
     if [ -d /home/$username ]; then makehome=""; fi
-    userdel $username && echo "User already existed, removing..."
-    groupdel $username && echo "Group already existed, removing..."
+    grep -q "^$username:" /etc/passwd && userdel $username && echo "User $username already existed, removing..."
+    grep -q "^$username:" /etc/group && groupdel $username && echo "Group $username already existed, removing..."
     useradd -N -g $gid $groupadd $makehome $skelarg -o -u $uid "$username"
     #Make sure home directory for user is owned by user.
     chown $uid:$gid /home/$username
